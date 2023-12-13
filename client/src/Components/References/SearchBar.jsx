7aa04bf7e9c0980526f101a159c50b41e92dc150
Safwan-Mohammed <safwanhanif19@gmail.com>
2023-12-13 22:06:47 +0530

    Force Pushing

---
+++ client/src/Components/References/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Flex, Header, Input } from "@chakra-ui/react"
+
+export default function SearchBar(){
+    return(
+        <>
+            <div>
+                <Flex direction={'column'} justifyContent={'center'} alignItems={'center'}>
+                    <Header>
+                        ALL YOUR NOTES IN ONE PLACE!
+                    </Header>
+                    
+                </Flex>
+            </div>
+        </>
+    )
+}
