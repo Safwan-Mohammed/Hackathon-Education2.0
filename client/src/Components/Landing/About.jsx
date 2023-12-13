41d8d0be5763b920d801a3019a5fc7c98c3a856a
Sevitha27 <sevitha.n03@gmail.com>
2023-12-13 20:34:17 +0530

    media queries added

--- client/src/Components/Landing/About.jsx
+++ client/src/Components/Landing/About.jsx
@@ -1,28 +1,34 @@
 import React from "react";
-import { Box, Flex, Text } from "@chakra-ui/react"
-import education from "../../images/Background/education.jpg"
+import { Box, Flex, Text } from "@chakra-ui/react";
+import education from "../../images/Background/education.jpg";
 
 export default function About() {
-    return (
-        <>
-            <Box height={'100vh'} position="relative">
-                <Box
-                    backgroundImage={`url(${education})`}
-                    backgroundSize="cover"
-                    backgroundPosition="center"
-                    height="100%"
-                    width="100%"
-                />
-                <Flex direction={'column'} position={'absolute'} top={"20%"} left={'5%'} width={'50%'}>
-                    <Text color="white" fontSize={'6rem'} fontWeight={600}>
-                        Edify!
-                    </Text>
-                    <Text color="white" fontSize={'2.85rem'}>
-                    Revolutionizing Learning, Inspiring Futures Your Transformative Education Hub
-                    </Text>
-                </Flex>
-            </Box>
-
-        </>
-    )
+  return (
+    <Box
+      height={{ base: "100vh", md: "50vh" }}
+      position="relative"
+    >
+      <Box
+        backgroundImage={`url(${education})`}
+        backgroundSize="cover"
+        backgroundPosition="center"
+        height="100%"
+        width="100%"
+      />
+      <Flex
+        direction="column"
+        position="absolute"
+        top={{ base: "10%", md: "20%" }}
+        left={{ base: "5%", md: "10%" }}
+        width={{ base: "90%", md: "80%" }}
+      >
+        <Text color="white" fontSize={{ base: "3rem", md: "6rem" }} fontWeight={600}>
+          Edify!
+        </Text>
+        <Text color="white" fontSize={{ base: "1.5rem", md: "2.85rem" }}>
+          Revolutionizing Learning, Inspiring Futures Your Transformative Education Hub
+        </Text>
+      </Flex>
+    </Box>
+  );
 }
--- client/src/Components/Landing/Routing.jsx
+++ client/src/Components/Landing/Routing.jsx
@@ -1,31 +1,29 @@
 import React from "react";
-import { Flex, Image, Text, Heading } from "@chakra-ui/react"
-import networking from '../../images/Routing/networking.jpg'
-import resources from '../../images/Routing/resources.jpg'
-import liveClass from '../../images/Routing/liveClass.jpg'
+import { Flex, Image, Text, Heading, useMediaQuery } from "@chakra-ui/react";
+import networking from '../../images/Routing/networking.jpg';
+import resources from '../../images/Routing/resources.jpg';
+import liveClass from '../../images/Routing/liveClass.jpg';
 
 export default function Routing() {
-    return (
-        <>
-            <div>
-                <Flex mt={'4rem'} mb={'2rem'} justifyContent={'center'} alignItems={'center'} p={'1rem 1rem 3rem 1rem'} direction={'column'}>
-                    <Heading mb={'4rem'}>CLICK HERE TO EXPLORE! </Heading>
-                    <Flex width={'90%'} direction={'row'} flexWrap={'wrap'} justifyContent={'center'} alignItems={'center'} gap={'5rem'}>
-                        <Flex border={'1px solid white'} borderRadius={'1rem'} p={'1rem'} direction={'row'} justifyContent={'center'} alignItems={'center'} width={'30vw'} gap={'1rem'} boxShadow={'0.1px 0.1px 5px rgba(255, 255, 255)'} cursor={'pointer'} >
-                            <Image height={'10rem'} width={'10rem'} src={liveClass} objectFit={'contain'}></Image>
-                            <Text wordBreak={'keep-all'}>Discover live lectures on topics you love! Click to join now.</Text>
-                        </Flex>
-                        <Flex scale={1.2} border={'1px solid white'} borderRadius={'1rem'} p={'1rem'} direction={'row'} justifyContent={'center'} alignItems={'center'} width={'30vw'} gap={'1rem'} boxShadow={'0.1px 0.1px 5px rgba(255, 255, 255)'} cursor={'pointer'}>
-                            <Image height={'10rem'} width={'10rem'} src={resources} objectFit={'contain'}></Image>
-                            <Text wordBreak={'keep-all'}>Access high-quality notes on your preferred topics! Click here to explore and download</Text>
-                        </Flex>
-                        <Flex border={'1px solid white'} borderRadius={'1rem'} p={'1rem'} direction={'row'} justifyContent={'center'} alignItems={'center'} width={'30vw'} gap={'1rem'} boxShadow={'0.1px 0.1px 5px rgba(255, 255, 255)'} cursor={'pointer'}>
-                            <Image height={'10rem'} width={'10rem'} src={networking} objectFit={'contain'}></Image>
-                            <Text wordBreak={'keep-all'}>Connect with like-minded individuals for engaging discussions! Click here to join now.</Text>
-                        </Flex>
-                    </Flex>
-                </Flex>
-            </div>
-        </>
-    )
+  const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
+
+  return (
+    <Flex mt={'4rem'} mb={'2rem'} justifyContent={'center'} alignItems={'center'} p={'1rem 1rem 3rem 1rem'} direction={'column'}>
+      <Heading mb={'4rem'}>CLICK HERE TO EXPLORE! </Heading>
+      <Flex width={'90%'} direction={'row'} flexWrap={'wrap'} justifyContent={'center'} alignItems={'center'} gap={'5rem'}>
+        <Flex border={'1px solid white'} borderRadius={'1rem'} p={'1rem'} direction={'column'} justifyContent={'center'} alignItems={'center'} width={isLargerThan768 ? '30%' : '90%'} gap={'1rem'} boxShadow={'0.1px 0.1px 5px rgba(255, 255, 255)'} cursor={'pointer'} >
+          <Image height={'10rem'} width={'10rem'} src={liveClass} objectFit={'contain'} />
+          <Text wordBreak={'keep-all'}>Discover live lectures on topics you love! Click to join now.</Text>
+        </Flex>
+        <Flex border={'1px solid white'} borderRadius={'1rem'} p={'1rem'} direction={'column'} justifyContent={'center'} alignItems={'center'} width={isLargerThan768 ? '30%' : '90%'} gap={'1rem'} boxShadow={'0.1px 0.1px 5px rgba(255, 255, 255)'} cursor={'pointer'}>
+          <Image height={'10rem'} width={'10rem'} src={resources} objectFit={'contain'} />
+          <Text wordBreak={'keep-all'}>Access high-quality notes on your preferred topics! Click here to explore and download</Text>
+        </Flex>
+        <Flex border={'1px solid white'} borderRadius={'1rem'} p={'1rem'} direction={'column'} justifyContent={'center'} alignItems={'center'} width={isLargerThan768 ? '30%' : '90%'} gap={'1rem'} boxShadow={'0.1px 0.1px 5px rgba(255, 255, 255)'} cursor={'pointer'}>
+          <Image height={'10rem'} width={'10rem'} src={networking} objectFit={'contain'} />
+          <Text wordBreak={'keep-all'}>Connect with like-minded individuals for engaging discussions! Click here to join now.</Text>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
